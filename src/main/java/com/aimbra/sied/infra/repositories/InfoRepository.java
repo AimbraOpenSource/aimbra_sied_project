0ef62436bdba0573c0b1fa1d739476e069d903bd
Thiago Cunha <thiagosantoscunha@outlook.com>
2020-06-03 11:07:22 -0300

    Add doc

--- src/main/java/com/aimbra/sied/infra/repositories/InfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aimbra.sied.infra.repositories;
-
-import com.aimbra.sied.domain.sied.entities.InfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InfoRepository extends JpaRepository<InfoEntity, Integer> {
-}
